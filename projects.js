487cc67d4cb8c1244d5ab98b8e111f297313557b
Shikhar <132381772+shikharcookies@users.noreply.github.com>
2023-08-17 20:44:51 +0530

    firstcommit

---
+++ projects.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    title: "Hand Gesture Recognition",
+    description: "A software to recognise hand gestures, built using python.",
+    image: "./assets/handimg.jpeg",
+    github: "https://github.com/shikharcookies/hand-gesture-recognition",
+  },
+  {
+    title: "Spotify API",
+    description:
+      "A software that fetches data from the Spotify API, built using Python.",
+    image: "./assets/spotifyimg.jpeg",
+    github: "https://github.com/shikharcookies/spotify-api",
+  },
+  {
+    title: "Recipe API",
+    description:
+      "A software that fetches data from a recipe API, built using Python.",
+    image: "./assets/recipeimg.jpeg",
+    github: "https://github.com/shikharcookies/recipe-api",
+  },
+];
